b22c7ae533707465d558b63fd0f8d67b3245dd07
Smog342 <mitro_02@mail.ru>
2024-08-01 15:14:41 +0300

    search field icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { SearchIcon } from "../icons/SearchIcon";
+
 export const Header = () => {
   return (
     <>
@@ -5,10 +7,13 @@ export const Header = () => {
         <p className="font-onest font-bold text-black text-[32px]/[40.8px] mr-auto">
           Список тестов
         </p>
-        <input
-          placeholder="Поиск"
-          className="w-[246px] h-[34px] rounded-[24px] border border-[#B1C5D380] border-opacity-50 ml-auto bg-inherit focus:outline-none py-[16px] px-[24px] placeholder:font-onest placeholder:font-normal placeholder:text-[16px]/[20.4px] placeholder:text-[#B1C5D3] font-onest font-normal text-[16px]/[20.4px] text-black"
-        ></input>
+        <div className="relative">
+          <SearchIcon />
+          <input
+            placeholder="Поиск"
+            className="w-[246px] h-[34px] rounded-[24px] border border-[#B1C5D380] border-opacity-50 ml-auto bg-inherit focus:outline-none py-[16px] px-[40px] placeholder:font-onest placeholder:font-normal placeholder:text-[16px]/[20.4px] placeholder:text-[#B1C5D3] font-onest font-normal text-[16px]/[20.4px] text-black"
+          ></input>
+        </div>
       </div>
     </>
   );
